79c7a23ad2df2c71271aeffc5ab46cddb573d002
Adam <adam@imt-atlantique.net>
2023-11-21 14:08:58 +0100

    mise à jour TP REST

--- TP - REST/booking/booking.py
+++ TP - REST/booking/booking.py
@@ -1,27 +1,37 @@
-from flask import Flask, render_template, request, jsonify, make_response
+### Booking service ###
+
+### Import des librairies. ###
+from flask import Flask, request, jsonify, make_response
 import requests
 import json
-from types import SimpleNamespace
-from werkzeug.exceptions import NotFound
 
+### Initialisation du serveur Flask ###
 app = Flask(__name__)
 
 PORT = 3201
 HOST = '0.0.0.0'
 
+### Ici on va chercher les données dans le fichier JSON. ###
 with open('{}/databases/bookings.json'.format("."), "r") as jsf:
-   bookings = json.load(jsf)["bookings"]
+    bookings = json.load(jsf)["bookings"]
 
+
+### Initialisation du serveur gRPC ###
+
+# Route pour la page d'accueil.
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
+
+# Route pour récupérer toutes les réservations.
 @app.route("/bookings", methods=['GET'])
 def get_json():
-   res = make_response(jsonify(bookings), 200)
-   return res
+    res = make_response(jsonify(bookings), 200)
+    return res
 
 
+# Route pour récupérer une réservation par son l'id d'un User.
 @app.route("/bookings/<userid>", methods=['GET'])
 def get_booking_for_user(userid):
     res = {}
@@ -29,10 +39,12 @@ def get_booking_for_user(userid):
         if str(user["userid"]) == str(userid):
             res.setdefault("bookings", []).append(user)
     if len(res) == 0:
-        return make_response(jsonify({"error":"User not found"}),400)
+        return make_response(jsonify({"error": "User not found"}), 400)
     else:
-        return make_response(jsonify(res),200)
+        return make_response(jsonify(res), 200)
 
+
+# Route pour ajouter une réservation à un User.
 @app.route("/bookings/<userid>", methods=['POST'])
 def add_booking_byuser(userid):
     req = request.get_json()
@@ -45,7 +57,7 @@ def add_booking_byuser(userid):
                 find = True
 
     if not find:
-       return make_response(jsonify({"error": "Film for this date not found"}), 400)
+        return make_response(jsonify({"error": "Film for this date not found"}), 400)
 
     bookings.append({
         "userid": userid,
@@ -58,10 +70,11 @@ def add_booking_byuser(userid):
             }
         ]
     })
-    res = make_response(jsonify({"message":"booking added"}),200)
+    res = make_response(jsonify({"message": "booking added"}), 200)
     return res
 
 
+### Lancement du serveur Flask ###
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
